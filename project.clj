9a76c29b253951fba49d3e06719fcf8ad43ac2cb
bhois <bernd.hois@infonova.com>
2017-11-16 13:21:46 +0100

    Set some deps to provided to lower footprint

--- project.clj
+++ project.clj
@@ -3,12 +3,13 @@
   :url "http://github.com/samplerr/samplerr"
   :license {:name "EPL-1.0"
             :url "https://spdx.org/licenses/EPL-1.0.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [cheshire "5.7.0"]
-                 [clj-time "0.13.0"]
-                 [riemann "0.2.13"]
-                 [org.clojure/tools.logging "0.3.1"]
-                 [cc.qbits/spandex "0.5.1"]]
+  :profiles {:provided
+             {:dependencies [[org.clojure/clojure "1.8.0"]
+                             [cheshire "5.7.1"]
+                             [clj-time "0.13.0"]
+                             [riemann "0.2.14"]
+                             [org.clojure/tools.logging "0.3.1"]]}}
+  :dependencies [[cc.qbits/spandex "0.5.1" :scope "provided"]]
   :plugins [[lein-rpm "0.0.5"
              :exclusions [org.apache.maven/maven-plugin-api
                           org.codehaus.plexus/plexus-container-default
