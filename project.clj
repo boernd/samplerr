c73c2cb71d48b2096ce8c1673eef594a831ff7b8
bhois <bernd.hois@infonova.com>
2017-11-16 13:22:57 +0100

    Set some deps to provided to lower footprint

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
                              [clj-time "0.13.0"]
                              [riemann "0.2.14"]
                              [org.clojure/tools.logging "0.3.1"]]}}
-  :dependencies [[cc.qbits/spandex "0.5.1" :scope "provided"]]
+  :dependencies [[cc.qbits/spandex "0.5.1"]]
   :plugins [[lein-rpm "0.0.5"
              :exclusions [org.apache.maven/maven-plugin-api
                           org.codehaus.plexus/plexus-container-default
